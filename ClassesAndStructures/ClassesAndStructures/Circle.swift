01254959b0fa01a1f9e9b52742cac4099bc100bd
Radi <radishikerov@yahoo.com>
2015-04-17 01:50:38 +0300

    Circle

--- ClassesAndStructures/ClassesAndStructures/Circle.swift
+++ ClassesAndStructures/ClassesAndStructures/Circle.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+class Circle: Shape {
+    var radius: Double
+    
+    init(radius: Double, name: String) {
+        self.radius = radius
+        super.init(name: name)
+        numberOfSides = 1
+    }
+    
+    override func description() -> String {
+        return "A circle with perimeter of \(self.radius * 2 * 3.14)"
+    }
+    
+    func calcArea() -> Double {
+        return 3.14 * self.radius * 2
+    }
+}
