2a3546017ac720a97e49658207bfa853d231051d
Radi <radishikerov@yahoo.com>
2016-08-24 13:47:05 +0300

    Complete add/fetch core data functionality

--- CoreData/Proj1-HitList/Proj1-HitList/ViewController.swift
+++ CoreData/Proj1-HitList/Proj1-HitList/ViewController.swift
@@ -8,11 +8,84 @@
 
 import UIKit
 
+import CoreData
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var people = [NSManagedObject]()
+    
+    private let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    let cellReuseID = "ListCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "The List"
+        self.tableView.registerClass(UITableViewCell.self,
+                                     forCellReuseIdentifier: self.cellReuseID)
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let managedContext = self.appDelegate.managedObjectContext
+        let fetchRequest = NSFetchRequest(entityName: "Person")
+        do {
+            let results = try managedContext.executeFetchRequest(fetchRequest)
+            self.people = results as! [NSManagedObject]
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    @IBAction func addName(sender: AnyObject) {
+        let alert = UIAlertController(title: "New Name",
+                                      message: "Add a new name",
+                                      preferredStyle: .Alert)
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .Default,
+                                       handler:
+            { (action:UIAlertAction) -> Void in
+                let textField = alert.textFields!.first
+                self.saveName(textField!.text!)
+                self.tableView.reloadData()
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .Default)
+        { (action: UIAlertAction) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler
+            { (textField: UITextField) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert,
+                              animated: true,
+                              completion: nil)
+    }
+    
+    func saveName(name: String) {
+        let managedContext = self.appDelegate.managedObjectContext
+        let entity = NSEntityDescription.entityForName("Person",
+                                                       inManagedObjectContext:managedContext)
+        let person = NSManagedObject(entity: entity!,
+                                     insertIntoManagedObjectContext: managedContext)
+        person.setValue(name, forKey: "name")
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save \(error), \(error.userInfo)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +94,20 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController : UITableViewDataSource {
+    func tableView(tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(tableView: UITableView,
+                   cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(self.cellReuseID)
+        let person = people[indexPath.row]
+        cell!.textLabel!.text = person.valueForKey("name") as? String
+        return cell!
+    }
 }
 
