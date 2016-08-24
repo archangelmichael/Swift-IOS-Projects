253945fc080680ba60230dd75fc5ad04bd281a38
Radi <radishikerov@yahoo.com>
2016-08-24 11:23:07 +0300

    Created a basic core data project

---
+++ CoreData/Proj1-HitList/Proj1-HitList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proj1-HitList
+//
+//  Created by Radi on 8/24/16.
+//  Copyright © 2016 archangel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
